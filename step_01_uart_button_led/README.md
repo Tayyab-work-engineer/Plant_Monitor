6b80d125c3f9ebc722482da8f18a0edca12b16e1
Tayyab Baig <tayyab@Tayyabs-iMac.local>
2025-07-25 16:53:38 +0100

    Add Step 01: UART, Button, LED control with AVR C

---
+++ step_01_uart_button_led/README.md
@@ -0,0 +1,20 @@
+# Step 01 – UART, Button & LED (Bare Metal AVR C)
+
+✅ Low-level UART setup (no Arduino Serial library)  
+✅ Direct port manipulation using `DDRB`, `PORTB`, `PIND`, etc.  
+✅ Button input with internal pull-up resistor  
+✅ Software debounce using `_delay_ms()`  
+✅ LED toggles ON/OFF with button press  
+✅ UART message prints: "LED ON"
+
+This is the first step in building my **Smart Plant Monitor with Adaptive Learning** project. Each folder in this repo represents a milestone in my learning journey.
+
+## Code Summary
+
+- `main.c`: Contains the full C code for button detection and UART feedback using ATmega328p.
+
+---
+
+🔜 **Next Step**: Interfacing with sensors, using structs for plant profiles, and categorizing simulated sensor readings.
+
+
---
+++ step_01_uart_button_led/main.c
@@ -0,0 +1,79 @@
+<avr/io.h>
+#include <string.h>
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <util/delay.h>
+
+#define BAUD 9600
+#include <util/setbaud.h>
+
+void uart_init(void){
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    
+    //character size 8
+    UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);
+    
+    UCSR0B = (1<<TXEN0);
+}
+
+void uart_transmit(unsigned char data){
+    while ( !(UCSR0A & (1<<UDRE0))); //wait to get ready to transmit
+    UDR0 = data;
+}
+
+void print_ln(const char* str){
+    int length = strlen(str);
+    int i;
+
+    for (i=0;i<length;i++){
+        uart_transmit(str[i]);
+    }
+    
+    uart_transmit(10); //new line
+
+
+}
+
+int main(void){
+    uart_init();
+    //uint8_t is
+    uint8_t buttonPrev = 1;  // Previous state of the button (1 = not pressed)
+
+    
+    DDRB = (1<<PB5); //Set pin 5 as output
+    PORTB = 0;
+    
+    DDRD = 0; //Set Port D as input
+    PORTD |= (1<<PD6); // Pull up resistor
+    
+    while(1){
+        uint8_t buttonNow;
+        
+        if (PIND & (1<<PD6)){
+            buttonNow = 1;
+        }else{
+            buttonNow = 0;
+        }
+        
+        
+        if ( buttonPrev == 1 && buttonNow == 0 ){
+            PORTB = (1<<PB5);
+            print_ln("LED ON");
+        }
+        else if (buttonPrev == 0 && buttonNow == 1){
+            PORTB = (0<<PB5);
+            buttonPrev = 1;
+
+        }
+        
+        buttonPrev = buttonNow;
+        _delay_ms(20); // Debounce delay
+
+    }
+    
+    return 0;
+}
+
+
